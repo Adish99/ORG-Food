4e469bf9d2fb4d83cf3653c297202782f9bc20ad
Adish99 <karkiadish999@gmail.com>
2026-09-08 19:48:44 +0545

    Final debug 10.

--- client/src/pages/Logout.jsx
+++ client/src/pages/Logout.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import {useNavigate} from "react-router-dom";
 import { UseAuth } from "../store/Authentication";
 
+//Logout component
 export const Logout=()=>{
 const {userLogout}=UseAuth();
 const navigate=useNavigate();
