b70e93e5808f001830a903ee54fb242a1a89a07d
Adish99 <karkiadish999@gmail.com>
2026-09-14 20:00:51 +0545

    Final debug 17.

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -3,6 +3,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "./Auth.css";
 
+//Reset Password Component
 export const ResetPassword = () => {
 
     const navigate = useNavigate();
